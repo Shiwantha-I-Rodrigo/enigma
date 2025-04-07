a2e7f5b2512de470a75c73b9e27303f062fc2a07
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2025-04-07 12:12:46 +0200

    increase_key_size

--- enigma_hasher.py
+++ enigma_hasher.py
@@ -1,29 +1,23 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from tqdm import tqdm
-import os, re, secrets, argon2
-
+import argon2
 
 #pip install cryptography
 #pip install argon2-cffi
-#pip install tqdm
-
 
 def hash_bytes(secret_1, secret_2):
     try:
         secret_2_bytes = secret_2.encode()
 
-        while len(secret_2_bytes) < 16:
+        while len(secret_2_bytes) < 32:
             secret_2_bytes += b'\x00'
         else:
-            secret_2_bytes = secret_2_bytes[:16]
+            secret_2_bytes = secret_2_bytes[:32]
 
         key_dev = argon2.PasswordHasher(
-            time_cost=1,
-            memory_cost=2*1048576,
+            time_cost=20,
+            memory_cost=3*1048576,
             parallelism=2,
             hash_len=512,
-            salt_len=16,
+            salt_len=32,
             type=argon2.low_level.Type.ID
         )
         print("deriving key...")
@@ -41,35 +35,13 @@ def hasher(secret_1, secret_2):
         key_str = hash_bytes(secret_1,secret_2).decode()
         key_str = key_str.replace("/","@")
         key_str = key_str.replace("+","*")
-        pass_length = ((len(secret_1)*len(secret_2))%16)+128
-        do_flag = True
-        while do_flag == True:
-            for i in range(0,len(key_str)-pass_length):
-                    match_l = re.search(r'[a-z]',key_str[i:i+pass_length])
-                    match_u = re.search(r'[a-z]',key_str[i:i+pass_length])
-                    match_n = re.search(r'[0-9]',key_str[i:i+pass_length])
-                    match_s = re.compile(r'[^a-zA-Z0-9]').search(key_str[i:i+pass_length])
-                    if match_l and match_u and match_n and match_s:
-                        print("\n")
-                        print(key_str[i:i+pass_length])
-                        print("\n")
-                        do_flag = False
-                        break
-            if do_flag:
-                key_str = hash_bytes(key_str, key_str).decode()
+        pass_length = 511
+        print("\n")
+        print(key_str[0:pass_length])
+        print("\n")
     except Exception as e:
         print(f"Exception ! - hasher - {e}")
         exit()
-
-
-def encryptor(input_file, secret_1, secret_2):
-    key = hash_bytes(secret_1, secret_2)
-    key = key[-32:]
-
-
-def decryptor(input_file, secret_1, secret_2):
-    key = hash_bytes(secret_1, secret_2)
-    key = key[-32:]
 
 
 print("\n\nenigma_hasher by shiva_the_cryptic")
